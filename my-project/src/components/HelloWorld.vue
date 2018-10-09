d75da9e47fc851b5aa043bfcf406744f7a3872f3
Leonardo_zyh <956731563@qq.com>
2018-10-09 22:39:25 +0800

    vue-cli

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>Hellow World</h1>
+    <h2>{{ msg }}</h2>
+    <h2>{{$route.params.msg}}</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  h1{color: red;}
+</style>
